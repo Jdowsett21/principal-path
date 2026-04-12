480203fc4e17241b6e713c6519d8988302e4e7eb
Jdowsett21 <james.dowsett.cooper@gmail.com>
2026-04-12 14:50:55 -0400

    Add PWA web install metadata

---
+++ app/+html.tsx
@@ -0,0 +1,23 @@
+export default function Html() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content="#f6f1e8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <title>Principal Path</title>
+      </head>
+      <body>
+        <div id="root" />
+      </body>
+    </html>
+  );
+}
